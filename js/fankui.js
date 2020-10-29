92649aaafa63a758400e291ea2a638b55c6126e9
adobe-jpg <60564873+adobe-jpg@users.noreply.github.com>
2020-10-29 19:49:42 +0800

    Add files via upload

---
+++ js/fankui.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+function formSubmit()
+{
+document.getElementById("myForm").submit()
+}
---
+++ js/kejizhanyi.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function bgChange(bg) {
+  document.body.style.background = bg;
+}
+
+function fun1(){
+	alert("这里就是首页哦")
+}
